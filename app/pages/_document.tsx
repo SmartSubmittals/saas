f494d3efc41f3a46218057477403dd6a5a1131c6
brdlysmth <brsmith110@gmail.com>
2020-11-15 14:27:41 -0500

    styling edits

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,6 +1,6 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
-import { ServerStyleSheets } from '@material-ui/core/styles';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheets } from '@material-ui/styles';
 
 class MyDocument extends Document {
   public static getInitialProps = async (ctx) => {
@@ -15,15 +15,9 @@ class MyDocument extends Document {
 
     const initialProps = await Document.getInitialProps(ctx);
 
-    console.log(initialProps);
-    console.log(initialProps.styles);
-    console.log(React.Children.toArray(initialProps.styles));
-
-    console.log(sheets);
-    console.log(sheets.getStyleElement());
-
     return {
       ...initialProps,
+      // Styles fragment is rendered after the app and page rendering finish.
       styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
     };
   };
@@ -38,6 +32,89 @@ class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta name="google" content="notranslate" />
           <meta name="theme-color" content="#303030" />
+
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400:latin"
+          />
+
+          <link
+            rel="shortcut icon"
+            href="https://storage.googleapis.com/async-await/async-favicon32.png"
+          />
+
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+          <link rel="stylesheet" href="https://storage.googleapis.com/async-await/vs2015.min.css" />
+
+          <link
+            rel="stylesheet"
+            href="https://storage.googleapis.com/async-await/nprogress-light.min.css"
+          />
+
+          <style>
+            {`
+              a,
+              a:focus {
+                font-weight: 600;
+                color: #000;
+                text-decoration: none;
+                outline: none;
+              }
+              a:hover,
+              button:hover {
+                opacity: 0.6;
+                cursor: pointer;
+              }
+              hr {
+                border: 0.5px #707070 solid;
+                color: #000;
+              }
+              blockquote {
+                padding: 0 0.5em;
+                margin: 20px 1em;
+                border-left: 0.25em solid #dfe2e5;
+                color: #000;
+              }
+              pre {
+                display: block;
+                overflow-x: auto;
+                padding: 0.5em;
+                background: #d0d0d0;
+                border: 1px solid #ddd;
+                font-size: 14px;
+                color: #000;
+              }
+              pre code {
+                font-size: 13px;
+                background: #d0d0d0;
+                padding: 0px;
+                color: #000;
+              }
+              code {
+                font-size: 13px;
+                background: #d0d0d0;
+                padding: 3px 5px;
+                color: #000;
+              }
+              mark {
+                background-color: #ffff0060;
+              }
+              summary:focus {
+                outline: none;
+              }
+              table {
+                border-collapse: collapse;
+                margin: 15px 0px;
+              }
+              table, th, td {
+                border: 1px solid #a1a1a1;
+              }
+              th, td {
+                line-height: 1.5em;
+                padding: 10px;
+              }
+            `}
+          </style>
         </Head>
         <body>
           <Main />
