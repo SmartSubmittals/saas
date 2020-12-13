a6b21491e5b5fc7635a0eeaa51c30789f9afec32
brdlysmth <brsmith110@gmail.com>
2020-12-13 15:35:30 -0500

    finished but lambda not working as expected...

--- app/lib/api/team-member.ts
+++ app/lib/api/team-member.ts
@@ -6,14 +6,12 @@ const BASE_PATH = '/api/v1/team-member';
  * the request's body contains all information (fileName, fileType, prefix, bucket) required by the AWS S3 server to generate a signed request that we need to successfully upload a file.
  * on our API server, the Express route /aws/get-signed-request-for-upload-to-s3 gets a request from APP and calls a handler function.
  */
+
 export const getSignedRequestForUploadApiMethod = ({ fileName, fileType, prefix, bucket }) =>
   sendRequestAndGetResponse(`${BASE_PATH}/aws/get-signed-request-for-upload-to-s3`, {
     body: JSON.stringify({ fileName, fileType, prefix, bucket }),
   });
 
-/**
- * uploads a file using a signed request.
- */
 export const uploadFileUsingSignedPutRequestApiMethod = (file, signedRequest, headers = {}) =>
   sendRequestAndGetResponse(signedRequest, {
     externalServer: true,
@@ -22,17 +20,11 @@ export const uploadFileUsingSignedPutRequestApiMethod = (file, signedRequest, he
     headers,
   });
 
-/**
- * update profile information, need to be logged in so this was moved from public.ts
- */
 export const updateProfileApiMethod = (data) =>
   sendRequestAndGetResponse(`${BASE_PATH}/user/update-profile`, {
     body: JSON.stringify(data),
   });
 
-/**
- * toggle theme request to api server
- */
 export const toggleThemeApiMethod = (data) =>
   sendRequestAndGetResponse(`${BASE_PATH}/user/toggle-theme`, {
     body: JSON.stringify(data),
@@ -59,6 +51,8 @@ export const getTeamMembersApiMethod = (teamId: string) =>
     method: 'GET',
     qs: { teamId },
   });
+
+// Discussion and Post
 
 export const getDiscussionListApiMethod = (params): Promise<{ discussions: any[] }> =>
   sendRequestAndGetResponse(`${BASE_PATH}/discussions/list`, {
@@ -107,7 +101,6 @@ export const sendDataToLambdaApiMethod = (data) =>
     externalServer: true,
     body: JSON.stringify(data),
   });
-
 /**
  * Notes:
  * - We place the code to a team-member.ts file instead of public.ts, because uploading files is allowed only for logged-in users.
